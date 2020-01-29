434157d1dc5dfd2707956dad01e222a776d02886
ambisinistra <9372109@gmail.com>
2020-01-29 14:31:21 +0300

    watch my project

---
+++ Assets/Moving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Moving : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space");
+            rigidBody.AddRelativeForce(new Vector3(0,20,0));
+        }
+    }
+}
